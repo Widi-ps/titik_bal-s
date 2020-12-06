61d79374675917acd78b4908ded27cbd426ce760
Widi-ps <widyapujisaputro@gmail.com>
2020-12-06 11:15:09 +0700

    Add toggle and animation navbar

--- js/kuy.js
+++ js/kuy.js
@@ -1,3 +1,28 @@
+// responsive navbar
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  // toggle nav
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    // animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinksFade 0.5s ease forwards ${
+          index / 7 + 1
+        }s`;
+      }
+    });
+  });
+};
+
+navSlide();
+
 // jquery untuk light slider
 $(document).ready(function () {
   $("#autoWidth").lightSlider({
