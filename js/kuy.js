9dd045e965e4cb43a413a96d9a55373707cf8130
Widi-ps <widyapujisaputro@gmail.com>
2020-12-30 09:43:41 +0700

    add scroll behaviour, fixed navbar, animation scroll

--- js/kuy.js
+++ js/kuy.js
@@ -1,27 +1,68 @@
-// responsive navbar
-const navSlide = () => {
+//navbar fixed when scroll
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  let windowPosition = window.scrollY > 0;
+
+  header.classList.toggle("scrolling-active", windowPosition);
+});
+
+
+// navbar responsive
+function burgerClick() {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
 
-  // toggle nav
-  burger.addEventListener("click", () => {
-    nav.classList.toggle("nav-active");
-
-    // animate links
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinksFade 0.5s ease forwards ${
-          index / 7 + 1
-        }s`;
-      }
+  burger.addEventListener("click", function (event) {
+    nav.classList.toggle("open");
+    navLinks.forEach(function (link) {
+      link.classList.toggle("fade");
     });
   });
-};
+}
 
-navSlide();
+function setupEventHandlers() {
+  burgerClick();
+}
+
+function initialize() {
+  setupEventHandlers();
+}
+
+$(initialize);
+
+//testimonial
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+const slides = document.querySelectorAll(".slide");
+
+let index = 0;
+
+function nextSlide() {
+  index++;
+  if (index > slides.length - 1) {
+    index = 0;
+  }
+  slides.forEach((slide) => {
+    slide.style.display = "none";
+  });
+  slides[index].style.display = "flex";
+}
+
+function prevSlide() {
+  index--;
+  if (index < 0) {
+    index = slides.length - 1;
+  }
+  slides.forEach((slide) => {
+    slide.style.display = "none";
+  });
+  slides[index].style.display = "flex";
+}
+
+next.addEventListener("click", nextSlide);
+next.addEventListener("click", prevSlide);
+
 
 // jquery untuk light slider
 $(document).ready(function () {
@@ -33,3 +74,46 @@ $(document).ready(function () {
     },
   });
 });
+
+//animation for scroll
+AOS.init({
+  offsite: 400,
+  duration: 1000,
+});
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
